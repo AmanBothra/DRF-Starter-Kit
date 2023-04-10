b8662b26fb898ae4515b78ab4b4831a8666529fc
AmanBothra <amanbothra1777@gmail.com>
2023-04-11 00:21:34 +0530

    fix issue

--- lib/models.py
+++ lib/models.py
@@ -4,21 +4,21 @@ from model_utils.models import TimeStampedModel
 
 
 class BaseModel(TimeStampedModel):
-	ORDERING = ("-created",)
+    ORDERING = ("-created",)
 
-	created_by = models.CharField(verbose_name="Created by", max_length=100, blank=True, null=True)
-	updated_by = models.CharField(verbose_name="Updated by", max_length=100, blank=True, null=True)
+    created_by = models.CharField(verbose_name="Created by", max_length=100, blank=True, null=True)
+    updated_by = models.CharField(verbose_name="Updated by", max_length=100, blank=True, null=True)
 
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
 
-	@property
-	def added_on(self):
-		return self.created
+    @property
+    def added_on(self):
+        return self.created
 
-	@property
-	def updated_on(self):
-		return self.modified
+    @property
+    def updated_on(self):
+        return self.modified
 
     def save(self, *args, **kwargs):
         if self.pk:
@@ -47,16 +47,16 @@ class BaseModel(TimeStampedModel):
                 raw_id_fields.append(field.name)
         return raw_id_fields
 
-	@classmethod
-	def get_all_field_names(cls):
-		return [field.name for field in cls._meta.fields]
+    @classmethod
+    def get_all_field_names(cls):
+        return [field.name for field in cls._meta.fields]
 
-	@classmethod
-	def get_list_filter_fields(cls, *args):
-		list_filter = ["created", "modified"]
-		exclude_fields = (arg for arg in args)
-		for field in cls._meta.get_fields():
-			if (isinstance(field, BooleanField) or (
-				isinstance(field, CharField) and field.choices)) and field.name not in exclude_fields:
-				list_filter.append(field.name)
-		return list_filter
+    @classmethod
+    def get_list_filter_fields(cls, *args):
+        list_filter = ["created", "modified"]
+        exclude_fields = (arg for arg in args)
+        for field in cls._meta.get_fields():
+            if (isinstance(field, BooleanField) or (
+                    isinstance(field, CharField) and field.choices)) and field.name not in exclude_fields:
+                list_filter.append(field.name)
+        return list_filter
